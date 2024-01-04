eaf9170c5ad8b45aee8d2ed4c3c82f94f4ef61ea
Vijaybhatia2208 <shanubhatia123@gmail.com>
2024-01-04 22:24:59 +0530

    Added code of Meme generator

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import './style.css';
+
+const element = document.getElementById('root');
+const root = ReactDOM.createRoot(element);
+
+root.render(
+    <center>
+        <App />
+    </center>
+);
